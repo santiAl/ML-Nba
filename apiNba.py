889c9f1983bf72f6f5be536f01f2b585d5ad19e4
santiAl <santialfonso2500@gmail.com>
2025-01-19 17:59:46 -0300

    Models modified and some styles added

--- apiNba.py
+++ apiNba.py
@@ -6,47 +6,27 @@ import json
 
 app = Flask(__name__)
 
-# Predictoras:    ['assists_x', 'blocks_x', 'offensive_rebounds_x', 'three_pointers_made_x', 'turnovers_x', 'field_goal_%_x', 'home_x', 'blocks_y', 'turnovers_y', 'record_y']
+# Predictoras:    ['field_goal_%_x', 'blocks_x', 'blocks_y', 'field_goal_%_y', 'recieved_field_goal_%_x', 'turnovers_y', 'recieved_points_x', 'score_x', 'recieved_field_goal_%_y', 'turnovers_x', 'recieved_points_y', 'score_y', 'record_y', 'home_x', 'record_x']
 
 # Cargar el modelo
-model = load('rrNbaModel.joblib')
-
-
-data = {
-    'assists_x': [20],
-    'blocks_x': [4],
-    'offensive_rebounds_x': [8],
-    'three_pointers_made_x': [13],
-    'turnovers_x': [3],
-    'field_goal_%_x': [0.25],
-    'home_x': [1],
-    'blocks_y': [3],
-    'turnovers_y': [2],
-    'record_y': [7]
-}
-
-# Crear un DataFrame
-input_data = pd.DataFrame(data)
-
-result = model.predict(input_data)
-print(result)
-
-
-
+model_rf = load('rfNbaModel.joblib')
+model_mlp = load('mlpNbaModel.joblib')
 
 # Ruta para servir el HTML
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+# Ruta para realizar predicciones
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json['data']
-    print(data)
-    data_df = pd.DataFrame(data)  # Convertir a DataFrame  
-    predicciones = model.predict(data_df).tolist()
-    print(predicciones)
+    model_selected = request.json['model']
+    data_df = pd.DataFrame(data)  # Convertir a DataFrame
+    if(model_selected == 0):
+        predicciones = model_rf.predict(data_df).tolist()
+    else:
+        predicciones = model_mlp.predict(data_df).tolist()
     return jsonify(predicciones)
 
 if __name__ == '__main__':
--- nba.py
+++ nba.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score , recall_score
 import matplotlib.pyplot as plt
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.feature_selection import SequentialFeatureSelector
@@ -14,10 +14,13 @@ from xgboost import XGBClassifier
 from joblib import dump
 
 # Configuración de conexión
+# Para traer los datos de la base de datos
+# La base de datos es local y se encuentra en mi computadora
+# Los datos de la DB fueron obtenidos a traves de la siguiente API:  https://rapidapi.com/frontalinilucas/api/nba-results-pro/playground/apiendpoint_475944cc-da4b-4a13-9dc3-00937e4d06b2
 config = {
-    'host': 'localhost',         # Ejemplo: 'localhost' o la IP del servidor
-    'user': 'root',      # Usuario de la base de datos
-    'database': 'nbadb' # Nombre de la base de datos
+    'host': 'localhost',         
+    'user': 'root',      
+    'database': 'nbadb' 
 }
 
 try:
@@ -30,7 +33,6 @@ try:
         # Crear un cursor para realizar consultas
         cursor = connection.cursor()
         
-        # Ejemplo de consulta
         cursor.execute("SELECT DATABASE();")
         result = cursor.fetchone()
         print("Base de datos actual:", result)
@@ -38,14 +40,23 @@ try:
 except mysql.connector.Error as e:
     print(f"Error al conectar a MySQL: {e}")
 
-
+# Se obtienen los datos llamando a un procedimiento almacenado
 cursor.callproc('get_table1')
 for result in cursor.stored_results():
         df = pd.DataFrame(result.fetchall(), columns=result.column_names)
 
+# Primero se verifican los tipos de las columnas
+print(df.dtypes)
+
+# Nuestra columna o variable target es la columna de tipo bool won_x y vamos a tratar de predecirla teniendo en cuenta los datos anteriores a ese partido. 
+
+# Se eliminan columnas duplicadas
 df = df.iloc[:, ~df.columns.duplicated()]
+# Se eliminan columnas innecesarias. En este caso son innnecesarias por que tienen muchos datos nulos.
 df = df.drop(columns=['away_steals', 'status','home_steals'])
+# Se convierte el tipo de la columna start_time_str a tipo fecha
 df['start_time_str'] = pd.to_datetime(df['start_time_str'],  format='%Y-%m-%d')
+# Se agregan columnas calculadas para mejorar la calidad de nuestro analisis
 df['away_field_goal_%'] = df['away_field_goals_made'] / df['away_field_goal_attempts']
 df['home_field_goal_%'] = df['home_field_goals_made'] / df['home_field_goal_attempts']
 df['away_free_throw_%'] = df['away_free_throws_made'] / df['away_free_throw_attempts']
@@ -53,17 +64,26 @@ df['home_free_throw_%'] = df['home_free_throws_made'] / df['home_free_throw_atte
 df['away_three_pointer_%'] = df['away_three_pointers_made'] / df['away_three_pointer_attempts']
 df['home_three_pointer_%'] = df['home_three_pointers_made'] / df['home_three_pointer_attempts']
 
+# Vemos el resumen estadistico para identificar como se comportan los datos
+print(df.describe())
 
-# Eliminacion datos atipicos.
+# Eliminacion datos atipicos.  Estos son partidos que todavia no se jugaron o fueron suspendidos.
 condition = (df['home_score'] == 0) | (df['away_score'] == 0)
 df = df[~condition]
+# En este caso estas columnas se eliminan por que tienen la mayoria de sus valores nulos o 0
 df = df.drop(columns=["home_personal_fouls","away_personal_fouls"])
 
-
+#---------------------------------------------------------------------- A continuacion estan las definiciones de algunas funciones importantes para el funcionamiento del modelo   -------------------------------------------------------------------------# 
+# Funcion diseñada para entrenar y testear los diferentes modelos
+# Parametros:
+# data: DataFrame con los datos
+# model: Modelo a entrenar
+# predictors: Columnas predictoras
+# size: Porcentaje de datos para entrenamiento
+# Retorna: DataFrame con las predicciones y el valor real
 def test(data,model,predictors,size):
-    # Porcentaje de datos para entrenamiento
+    
     train_size = size
-
     # Calcular el índice de corte entrenamiento
     train_index = int(len(data) * train_size)
 
@@ -76,21 +96,21 @@ def test(data,model,predictors,size):
     preds = pd.Series(preds,index=test.index)
 
     combined = pd.concat([test['won_x'],preds], axis = 1)
-    combined.columns = ["actual","prediction"]   #Renombremos las columnas
+    combined.columns = ["actual","prediction"]
 
     return combined
 
 
-
-def team_games(team):      # Hace una fila de cada partido segun el equipo
+# Dado un equipo devuelve un dataframe con los partidos de ese equipo y sus estadisticas posteriores a ese partido
+def team_games(team):      
     filtered_df = df[(df['away_team_name'] == team) | (df['home_team_name'] == team)]
-    filtered_df.sort_values(by='start_time_str', ascending=False, inplace=True)
+    filtered_df.sort_values(by='start_time_str', ascending=True, inplace=True)
     result = filtered_df.apply(new_df,axis=1,args={team,})
     result_df = pd.DataFrame(result.tolist())
 
     return result_df
 
-
+# Devuelve un diccionario con la info de un partido en concreto
 def new_df(row,team): # Cambia nombre de columnas (las ordena)
     if row['away_team_name'] == team:
         columns = df.filter(regex='^away').columns
@@ -102,6 +122,21 @@ def new_df(row,team): # Cambia nombre de columnas (las ordena)
     new_df_r = {}
     for index , col in enumerate(columns):
         new_df_r[new_column_names[index]] = row[col]
+
+    if(row["home_team_name"] == team):
+        new_df_r['recieved_points'] = row['away_score']
+        new_df_r['recieved_field_goal_%'] = row['away_field_goal_%']
+        #new_df_r['forced_turnovers'] = row['away_turnovers']
+        #new_df_r['recieved_free_throw_attemps%'] = row['away_free_throw_attempts']
+        new_df_r['recieved_offensive_rebounds'] = row['away_offensive_rebounds']
+    else:
+        new_df_r['recieved_points'] = row['home_score']
+        new_df_r['recieved_field_goal_%'] = row['home_field_goal_%']
+        #new_df_r['forced_turnovers'] = row['home_turnovers']
+        #new_df_r['recieved_free_throw_attemps%'] = row['home_free_throw_attempts']
+        new_df_r['recieved_offensive_rebounds'] = row['home_offensive_rebounds']
+
+
     
     if(row["home_team_name"] == team):
         if(row['home_score'] > row['away_score']):
@@ -125,22 +160,22 @@ def new_df(row,team): # Cambia nombre de columnas (las ordena)
     return new_df_r
     
 
-
+# Funcion definida para sacar las estadisticas del equipo antes de jugar ese partido
+# Se sacan las estadisticas de los ultimos 10 partidos
+# Por ejemplo: promedio de puntos en los ultimos 10 partidos , promedio de rebotes en los ultimos 10 partidos , etc
 def find_team_averages(team):
-    shifted_team = team.shift(1)       # Para sacar las estadisticas del equipo antes de jugar ese partido
-    rolling = shifted_team.rolling(10).mean()    # Aqui cambiar este numero para encontar las estadisticas de los ultimos n partidos
+    shifted_team = team.shift(1)       
+    rolling = shifted_team.rolling(10).mean()    
     return rolling
 
-def find_record(team):
-    count = team["won"].rolling(2).sum()
-    return count
-
+# Funcion utilizada para encontrar las n caracteristicas mas importantes para el modelo random forest
+# Retorna las n caracteristicas mas importantes
 def find_best_features_rf(data,n=None):
     full_two = data.copy()
-    rf_two = RandomForestClassifier(n_estimators=200, random_state=43,max_depth=15,min_samples_leaf=40)
+    rf_two = RandomForestClassifier(n_estimators=350, random_state=43,max_depth=10,min_samples_leaf=60,min_samples_split=100)
     train_index = int(len(full_two) * 0.7)
     train = full_two[:train_index]
-    x_train = train.drop(columns=['won_x','start_time_str',"game_id_x"])
+    x_train = train.drop(columns=['won_x','start_time_str'])
     y_train = train['won_x']
     
     rf_two.fit(x_train, y_train)
@@ -158,6 +193,7 @@ def find_best_features_rf(data,n=None):
 
     return selected_column_names
 
+#--------------------------------------------------------------- Fin de las definiciones de algunas funciones -----------------------------------------------------------------------------------------------------
 
 df_combined = pd.DataFrame()
 
@@ -166,18 +202,17 @@ for team in df['home_team_name'].unique():
     removed_columns = ['period','team_name']
     current_team = team_games(team)
     selected_columns = current_team.columns[~current_team.columns.isin(removed_columns)]           
-
+    
     rolling_df = current_team[selected_columns]
 
-    
+    #-------------------- Calcular las estadisticas acumuladas en toda la temporada.  ----------------------------------
     rolling_df['cumulative_made'] = rolling_df['field_goals_made'].cumsum() - rolling_df['field_goals_made']
     rolling_df['cumulative_attempted'] = rolling_df['field_goal_attempts'].cumsum() - rolling_df['field_goal_attempts']
-
-    # Calcular el porcentaje acumulado hasta el partido anterior
     rolling_df['cumulative_percentage'] = (rolling_df['cumulative_made'] / rolling_df['cumulative_attempted'])
-
+    #-------------------- Calcular las estadisticas acumuladas en toda la temporada.  ----------------------------------
     
-    # Extraer la columnas (que no quiero que se les saque el porcentaje) antes del cálculo
+
+    # Extraer la columnas que no es necesario que se les saque el promedio de los ultimos n paridos
     game_id = rolling_df['game_id']
     home = rolling_df['home']
     oponent = rolling_df['oponent_id']
@@ -185,81 +220,95 @@ for team in df['home_team_name'].unique():
     start_time = rolling_df['start_time_str']
     percentage = rolling_df['cumulative_percentage']
 
-    # Eliminar columnas temporalmente
+    # Eliminar las mismas columnas temporalmente
     rolling_df = rolling_df.drop(columns=['game_id',"home","oponent_id","start_time_str","won",'cumulative_made','cumulative_attempted','cumulative_percentage'])
 
-    rolling_df = rolling_df.groupby(['team_id'] , group_keys = False).apply(find_team_averages)   # Calculo del promedio de estadisticas de los ultimos n partidos
+    # Calculo del promedio de estadisticas de los ultimos n partidos
+    rolling_df = rolling_df.groupby(['team_id'] , group_keys = False).apply(find_team_averages)
     
-    # Reinsertar columnas después del cálculo
+    # Reinsertar columnas eliminadas después del cálculo
     rolling_df['won'] = won
     rolling_df['game_id'] = game_id
     rolling_df['home'] = home
     rolling_df['oponent_id'] = oponent
     rolling_df['start_time_str'] = start_time
-    #print(rolling_df)
+    #------------------------------------------ Agregamos la columna record (partidos ganados - partidos perdidos [en toda la temporada])  ------------------------------------------#
     rolling_df['matches_win'] = rolling_df['won'].cumsum()
     rolling_df['matches_win'] = rolling_df.apply(lambda row: row['matches_win'] - 1 if row['won'] else row['matches_win'], axis=1)
     rolling_df['matches_defeat'] = (rolling_df['won'] == 0).cumsum()
     rolling_df['matches_defeat'] = rolling_df.apply(lambda row: row['matches_defeat'] if row['won'] else row['matches_defeat'] - 1, axis=1)
     rolling_df['record'] = rolling_df['matches_win'] - rolling_df['matches_defeat']
+    #------------------------------------------ Agregamos la columna record (partidos ganados - partidos perdidos [en toda la temporada])  ------------------------------------------#
     rolling_df['cumulative_percentage'] = percentage
-    # Para las estadisticas del otro equipo podria sacar estas estadisticas para todos los equipos y luego hacer un merge.
 
+    # Vamos juntando los dataframes de cada equipo
     df_combined = pd.concat([df_combined, rolling_df], ignore_index=True)
 
 
-#Para tener las estadisticas de ambos equipos en la misma fila.
+# Para tener las estadisticas de ambos equipos antes del partido en la misma fila.
 full = df_combined.merge(df_combined, left_on=["team_id","start_time_str"], right_on=["oponent_id","start_time_str"])
 
-#print(full[(full['team_id_x']==42)][['true_count_last_10_x','true_count_last_10_y']])
 
+
+# Ordenamos los partidos por fecha por que vamos a querer predecir el resultado de un partido basado en resultados previos 
+#full = full.sort_values(by="start_time_str")
 full = full.sort_values(by="start_time_str")
-
-# Aca arriba saque varias columnas que estaban correlacionadas entre si y empreoraban el rendimiento de rf
-full = full.drop(columns=["oponent_id_y","game_id_y","home_y","won_y","team_id_y","free_throw_attempts_x","free_throw_attempts_y","three_pointer_attempts_x","three_pointer_attempts_y","field_goals_made_x","field_goals_made_y",'score_x','score_y','matches_defeat_y','matches_defeat_x','matches_win_x','matches_win_y'])   # Columnas que no me interesan
-
+# Luego de un analisis exploratorio, analizando la matriz de correlacion y los graficos de dispersion, decidi eliminar varias columnas que estaban muy correlacionadas entre si y aportaban informacion redundante.
+full = full.drop(columns=["oponent_id_y","game_id_y","home_y","won_y","team_id_y","free_throw_attempts_x","free_throw_attempts_y","three_pointer_attempts_x","three_pointer_attempts_y","field_goals_made_x","field_goals_made_y",'matches_defeat_y','matches_defeat_x','matches_win_x','matches_win_y','cumulative_percentage_y','cumulative_percentage_x'])   # Columnas que no me interesan
 
 
-removed_columns = list(full.columns[full.dtypes == 'object']) + ['won_x','team_id_x','oponent_id_x','start_time_str','game_id_x']
-selected_columns = full.columns[~full.columns.isin(removed_columns)]
+
 
 #-------------------------------- Definicion de los diferentes modelos -------------------------------------
 
 xgb_model = XGBClassifier(
-    n_estimators=100,  # Número de árboles
+    n_estimators=200,  # Número de árboles
     learning_rate=0.1,  # Tasa de aprendizaje
-    max_depth=3,       # Profundidad máxima de los árboles
+    max_depth=10,       # Profundidad máxima de los árboles
     random_state=42
 )
-rf = RandomForestClassifier(n_estimators=200, random_state=43,max_depth=10,min_samples_leaf=40)
+rf = RandomForestClassifier(n_estimators=350, random_state=43,max_depth=10,min_samples_leaf=60,min_samples_split=100)
 rr = RidgeClassifier(alpha=1)
+
 split = TimeSeriesSplit(n_splits=3)
-
-sfs = SequentialFeatureSelector(rr, n_features_to_select = 10, direction="backward", cv=split)   # Slecciona las mejores features (columnas)
-
-
-#-------------------------------- Definicion de los diferentes modelos -----------------------------------
+# El sequential feature selector selecciona las mejores features para el modelo
+sfs = SequentialFeatureSelector(rr, n_features_to_select = 5, direction="backward", cv=split)   # Slecciona las mejores features (columnas)
 
 
+#-------------------------------- Definicion de los diferentes modelos -------------------------------------------------------
+
+removed_columns = list(full.columns[full.dtypes == 'object']) + ['won_x','team_id_x','oponent_id_x','start_time_str','game_id_x']
+selected_columns = full.columns[~full.columns.isin(removed_columns)]
+
+full.to_csv("nbaData.csv", index=False)
 # Ridge funciona mejor estandatizando.
 scaler = MinMaxScaler()
 full[selected_columns] = scaler.fit_transform(full[selected_columns])   # Escalado de los datos menos los que estan en removed_columns
 full = full.dropna()
+print(full.dtypes)
+# Obtener los predictores seleccionados para rr 
 sfs.fit(full[selected_columns],full["won_x"])
-
-# Obtener los predictores seleccionados para rr
 predictors = list(selected_columns[sfs.get_support()])
 
 # Buscar los mejores predictores para random forest
-predictors_rf = find_best_features_rf(full,5)
+predictors_rf = find_best_features_rf(full,15)
 
+#full = full.drop(columns=['game_id_x'])   # Columnas que no me interesan
 predictions = test(full,rr,predictors,0.7)
 
-print(predictors)
+# Para vizualizar los predictores
+print(predictors_rf)
+# Calculo del accuracy para saber la bondad del ajuste
 acc = accuracy_score(predictions['actual'],predictions['prediction'])
+recall = recall_score(predictions['actual'], predictions['prediction'])
+# Para visualizar el accuracy
 print(acc)
+print(recall)
 
+# Guardar el modelo
 #dump(rr, 'rrNbaModel.joblib')
+
+
 
 # Para hacer graficos de dispersion.
 """
@@ -279,11 +328,25 @@ plt.show()
 # Para hacer una matriz de correlación
 """
 correlation_matrix = full.corr()
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(15, 15))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
 plt.title("Matriz de Correlación")
 plt.show()
 """
+"""
+# Configurar el tamaño del gráfico
+plt.figure(figsize=(15, 8))
+
+# Crear boxplots para cada variable numérica
+sns.boxplot(data=full[selected_columns])
+
+# Rotar etiquetas del eje X si hay muchas variables
+plt.xticks(rotation=90)
+
+plt.show()
+"""
+
+
 
 # Cerrar la conexión
 if connection.is_connected():
